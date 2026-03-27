f34eef3a11e2794ec6a8a21eabfd414aceaf1066
shivansh9794 <mshivansh549@gmail.com>
2026-03-27 22:22:24 +0530

    Fixed my cors origins

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,7 @@ app.use(cookieParser());
 // app.use(cors());
 app.use(
   cors({
-    // origin: "http://localhost:5173",
-    origin: "http://localhost:5174",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true
   })
 );
