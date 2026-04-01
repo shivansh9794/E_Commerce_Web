1eab6d929ab0bc69de52e574b4290e3e740c2b01
shivansh9794 <mshivansh549@gmail.com>
2026-04-01 23:09:50 +0530

    fixed admin route

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -4,6 +4,5 @@ import { userGrowth15days } from '../controller/adminController.js';
 const router = express.Router();
 
 router.get("/user-growth-15days", userGrowth15days);
-// router.get("/user-growth-15days", userGrowth15days);
 
 export default router;
